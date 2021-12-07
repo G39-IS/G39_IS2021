916c3e9b51c7cca06339db89da1dc303ede9adb6
jonathanmicheli <jonathan.micheli@studenti.unitn.it>
2021-12-07 10:44:16 +0100

    add index.js in api
    add index.js in api

---
+++ api/index.js
@@ -0,0 +1,106 @@
+const Express = require("express");
+const app = Express();
+
+// modules to generate APIs documentation
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Express API for My Project',
+            version: '1.0.0',
+            description:
+                'This is a REST API application made with Express.',
+            license: {
+                name: 'Licensed Under MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'Group39',
+                url: 'http://localhost:8080/',
+            },
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080/',
+                description: 'Development server',
+            },
+        ],
+    },
+    apis: ["index.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+
+var fs = require('fs');
+
+var cors = require('cors')
+app.use(cors())
+
+// module to parse the API body request
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.listen(8080, () => {
+    console.log("APIs Running");
+
+
+});
+
+
+/**
+ * @swagger
+ * /api/usersearch:User:
+ *   get:
+ *     summary: Retrieve a signle User.
+ *     description: etrieve a signle User from the Server.
+ *     responses:
+ *       200:
+ *         description: A User informations.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       Nome:
+ *                         type: integer
+ *                         description: The User Name.
+ *                         example: Giovanna
+ *                       Cognoome:
+ *                         type: string
+ *                         description: The User Surname.
+ *                         example: Barotti
+ *                       User:
+ *                          type: string
+ *                          description: The User username
+ *                          example: calligalli
+ */
+app.get('/api/usersearch:user', (request, response) => {
+    var data = fs.readFileSync('../assets/users.json');
+    var myObject = JSON.parse(data);
+    var returnObj;
+    for (let [i, us] of myObject.Users.entries()) {
+
+        if (us.User == request.params.user) {
+            returnObj = JSON.parse(us);
+        }
+    }
+
+    response.send(returnObj);
+
+})
+
