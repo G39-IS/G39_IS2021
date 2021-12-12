d7d52d6c89be02d28ffc29008bfb1ca9673a83d8
Agnieszka <95644855+Calliphora@users.noreply.github.com>
2021-12-12 17:17:31 +0100

    Update account.js

--- ui/account.js
+++ ui/account.js
@@ -1,1 +1,14 @@
-const account={template:`<h1>Account</h1>`}
+const account={template:`
+<div>
+<input type="text" id="name" onkeyup="myFunction()" placeholder="Cerca altri utenti...">
+<center><h1><i>Ciao, Mario Rossi!</i></h1></center>
+<center><h2>Il tuo username: <b>ABC123</b></h2></center>
+<br>
+<center><button type="button" class="btn btn-secondary btn-lg btn-block">Lista desideri</button></center>
+<br>
+<center><button type="button" class="btn btn-secondary btn-lg btn-block">Lista richieste follow</button></center>
+<br>
+<center><button type="button" class="btn btn-secondary btn-lg">Log out</button></center>
+</div>
+`
+}
