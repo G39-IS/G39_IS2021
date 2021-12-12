52b8e566fcb0fc8539343de42acc1ce15827fa11
Agnieszka <95644855+Calliphora@users.noreply.github.com>
2021-12-12 17:48:52 +0100

    Update ricerca.js

--- ui/ricerca.js
+++ ui/ricerca.js
@@ -1,1 +1,6 @@
-const ricerca={template:`<div class="header1 row d-flex justify-content-center">RICERCA</div>`}
+const ricerca={template:`<div>
+<div class="header1 row d-flex justify-content-center">RICERCA</div>
+<h3>Vuoi trovare un regalo per qualcuno?</3>
+<br>
+<h3>Purtroppo, al momento non ti possiamo aiutare, ma <b><a href="https://www.amazon.it/">Jeff Bezos</a></b>, sì... (NOT SPONSORED)</h3>
+</div>`}
