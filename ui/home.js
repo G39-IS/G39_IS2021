ac43c8917050da0611af6be84cef6ac6eb1ea2f5
Agnieszka <95644855+Calliphora@users.noreply.github.com>
2021-12-16 19:47:09 +0100

    Update home.js

--- ui/home.js
+++ ui/home.js
@@ -1,17 +1,63 @@
 const home={template:`
 <div>
 <div class="header1 row d-flex justify-content-center">HOME</div>
-<h1>Benvenuti nella casa dei widget</h1>
-<h2>Al momento ci vivono solo le immagini :-(</h2>
-<img src="widget1.png">
-<br><br>
-<img src="widget2.png">
-<br><br>
-<button>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-</svg>
-</button>
+<!--free widget code from https://www.bootdey.com/snippets/view/reminder-widget#html-->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-5">
+            <div class="widget widget-reminder">
+		<div class="widget-reminder-header"><b>EVENTI DELLA SETTIMANA</b></div>
+                <div class="widget-reminder-header">Oggi, 22. DICEMBRE 2021</div>
+                <div class="widget-reminder-container">
+                    <div class="widget-reminder-time">
+                        09:00<br />
+                        12:00
+                    </div>
+                    <div class="widget-reminder-divider bg-success"></div>
+                    <div class="widget-reminder-content">
+                        <h4 class="widget-title">Incontro in sala studio</h4>
+                        <div class="widget-desc">Preparazione per invio D5 per SE</div>
+                    </div>
+                </div>
+                <div class="widget-reminder-container">
+                    <div class="widget-reminder-time">
+                        20:00<br />
+                        23:00
+                    </div>
+                    <div class="widget-reminder-divider bg-primary"></div>
+                    <div class="widget-reminder-content">
+                        <h4 class="widget-title">Cena di Natale con amici</h4>
+                        <div class="widget-desc">Casa di Marco</div>
+                        <div class="m-t-15">
+                            <a href="#" class="pull-right">Account</a>
+                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" width="16" class="img-circle pull-left m-r-5" alt="" /> Marco Piervilla
+                        </div>
+                    </div>
+                </div>
+                <div class="widget-reminder-header">Domani, 23. DICEMBRE 2021</div>
+                <div class="widget-reminder-container">
+                    <div class="widget-reminder-time">Tutto il giorno</div>
+                    <div class="widget-reminder-divider bg-purple"></div>
+                    <div class="widget-reminder-content">
+                        <h4 class="widget-title">Preparazione di dolci</h4>
+                    </div>
+                </div>
+                <div class="widget-reminder-container">
+                    <div class="widget-reminder-time">
+                        21:00<br />
+                        21:30
+                    </div>
+                    <div class="widget-reminder-divider bg-danger"></div>
+                    <div class="widget-reminder-content">
+                        <h4 class="widget-title">Messaggi di auguri</h4>
+                        <div class="widget-desc"><i class="ti-pin"></i>Inviare gli auguri ai conoscenti</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<button type="button" class="btn btn-secondary">+</button>
 </div>
 
 `}
