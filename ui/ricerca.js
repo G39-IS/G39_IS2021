e44999c4af5814fff7bd84e6f9324158365c79e5
Agnieszka <95644855+Calliphora@users.noreply.github.com>
2021-12-16 20:44:56 +0100

    Update ricerca.js

--- ui/ricerca.js
+++ ui/ricerca.js
@@ -1,6 +1,35 @@
 const ricerca={template:`<div>
 <div class="header1 row d-flex justify-content-center">RICERCA</div>
-<h3>Vuoi trovare un regalo per qualcuno?</h3>
 <br>
-<h3>Purtroppo, al momento non ti possiamo aiutare, ma <b><a href="https://www.amazon.it/">Jeff Bezos</a></b>, sì... (NOT SPONSORED)</h3>
+<div>
+<center>
+<h5><i>Non hai idea cosa regalare? Inserisci le informazioni della persona alla quale vuoi fare un regalo:</i></h5>
+<br>
+Categoria persona:<br>
+<select v-model="selected">
+  <option disabled value="">-SELEZIONA CATEGORIA-</option>
+  <option>Uomo</option>
+  <option>Donna</option>
+  <option>Teenager</option>
+  <option>Bambino</option>
+</select><br><br>
+Prezzo:<br>
+<select v-model="selected">
+  <option disabled value="">-SELEZIONA FASCIA PREZZO-</option>
+  <option>Fino a 10 EUR</option>
+  <option>Fino a 50 EUR</option>
+  <option>Fino a 100 EUR</option>
+  <option>Oltre 100 EUR</option>
+</select><br><br>
+Categoria d'interesse:<br>
+<select v-model="selected" multiple>
+  <option>Elettronica</option>
+  <option>Cosmetici</option>
+  <option>Libri</option>
+  <option>Musica</option>
+</select><br><br>
+<input type=button class="btn btn-secondary" onClick="parent.open('https://www.amazon.it/')" 
+value='Chiedi un consiglio a Jeff Bezos' >
+</center>
+</div>
 </div>`}
